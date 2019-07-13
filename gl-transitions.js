529753ebaf5e7356f81ad4762368c3e36274e444
Oksana Plotnikova <oksankapl17@gmail.com>
2019-07-13 23:05:17 +0300

    Change default directional transition direction

--- gl-transitions.js
+++ gl-transitions.js
@@ -74,7 +74,7 @@ module.exports =
   }, {
     "name": "Directional",
     "paramsTypes": {"direction": "vec2"},
-    "defaultParams": {"direction": [0, 1]},
+    "defaultParams": {"direction": [0, -1]},
     "glsl": "// Author: Gaëtan Renaudeau\n// License: MIT\n\nuniform vec2 direction; // = vec2(0.0, 1.0)\n\nvec4 transition (vec2 uv) {\n  float easing = sqrt((2.0 - progress) * progress);\n  vec2 p = uv + easing * sign(direction);\n  vec2 f = fract(p);\n  return mix(\n    getToColor(f),\n    getFromColor(f),\n    step(0.0, p.y) * step(p.y, 1.0) * step(0.0, p.x) * step(p.x, 1.0)\n  );\n}\n",
     "author": "Gaëtan Renaudeau",
     "license": "MIT",
